7ec3a2e5590f572fd5f93c1f9f60a87343b95637
BadyBenSalahh <badybensalahh@gmail.com>
2018-05-02 01:24:40 +0100

    SwiftMailer

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,6 +15,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+        //---------------------------------------------------
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
 
@@ -149,4 +151,8 @@ class DefaultController extends Controller
         ]);
 
     }
+
+
+
+
 }
--- src/AppBundle/Controller/PlanController.php
+++ src/AppBundle/Controller/PlanController.php
@@ -1,14 +1,11 @@
 <?php
-
 namespace AppBundle\Controller;
-
 use AppBundle\Entity\Plan;
 use AppBundle\Entity\Media;
 use AppBundle\Entity\LikesPlan;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
-
 /**
  * Plan controller.
  *
@@ -24,15 +21,30 @@ class PlanController extends Controller
      */
     public function indexAction()
     {
+        $transport =(new \Swift_SmtpTransport('smtp.gmail.com'));
+        $transporft = \Swift_SmtpTransport::newInstance('smtp.gmail.com',465,'ssl');
+        $transporft->setUsername('bonplanbady@gmail.com');
+        $transporft->setPassword('bonplanbady01052018');
+        $transporft->setAuthMode('login');
+
+        $mailer = new \Swift_Mailer($transporft);
+
+        $message = (new \Swift_Message(('Wonderful aagaegaegaeg')))
+            ->setFrom(['bonplanbady@gmail.com'=>'BonPlanBady'])
+            ->setTo(['badi3.ben.salah@gmail.com'])
+            ->setBody('aegaegmljaegjf');
+
+        $result = $mailer->send($message);
+
+
+
+
         $em = $this->getDoctrine()->getManager();
-
         $plans = $em->getRepository('AppBundle:Plan')->findAll();
-
         return $this->render('plan/index.html.twig', array(
             'plans' => $plans,
         ));
     }
-
     /**
      * Finds and displays a article entity.
      *
@@ -55,7 +67,6 @@ class PlanController extends Controller
         $em->flush();
         return $this->redirectToRoute("experiences_detailed",['id' => $plan->getId()]);
     }
-
     /**
      * Finds and displays a article entity.
      *
@@ -78,7 +89,6 @@ class PlanController extends Controller
         $em->flush();
         return $this->redirectToRoute("experiences_detailed",['id' => $plan->getId()]);
     }
-
     /**
      * Creates a new plan entity.
      *
@@ -90,12 +100,10 @@ class PlanController extends Controller
         $plan = new Plan();
         $form = $this->createForm('AppBundle\Form\PlanType', $plan);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $file =  $request->files->get('file'); // eli mawjoude fel form input type=file
             $media = new Media();
             $media->setFile($file);
-
             $em = $this->getDoctrine()->getManager();
             $em->persist($media);
             $plan->addMedia($media);
@@ -103,16 +111,13 @@ class PlanController extends Controller
             $plan->setUserName($this->getUser());
             $em->persist($plan);
             $em->flush();
-
             return $this->redirectToRoute('plan_show', array('id' => $plan->getId()));
         }
-
         return $this->render('plan/new.html.twig', array(
             'plan' => $plan,
             'form' => $form->createView(),
         ));
     }
-
     /**
      * Finds and displays a plan entity.
      *
@@ -122,13 +127,11 @@ class PlanController extends Controller
     public function showAction(Plan $plan)
     {
         $deleteForm = $this->createDeleteForm($plan);
-
         return $this->render('plan/show.html.twig', array(
             'plan' => $plan,
             'delete_form' => $deleteForm->createView(),
         ));
     }
-
     /**
      * Displays a form to edit an existing plan entity.
      *
@@ -140,20 +143,16 @@ class PlanController extends Controller
         $deleteForm = $this->createDeleteForm($plan);
         $editForm = $this->createForm('AppBundle\Form\PlanType', $plan);
         $editForm->handleRequest($request);
-
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $this->getDoctrine()->getManager()->flush();
-
             return $this->redirectToRoute('plan_edit', array('id' => $plan->getId()));
         }
-
         return $this->render('plan/edit.html.twig', array(
             'plan' => $plan,
             'edit_form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
         ));
     }
-
     /**
      * Deletes a plan entity.
      *
@@ -164,16 +163,13 @@ class PlanController extends Controller
     {
         $form = $this->createDeleteForm($plan);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->remove($plan);
             $em->flush();
         }
-
         return $this->redirectToRoute('plan_index');
     }
-
     /**
      * Creates a form to delete a plan entity.
      *
@@ -187,6 +183,6 @@ class PlanController extends Controller
             ->setAction($this->generateUrl('plan_delete', array('id' => $plan->getId())))
             ->setMethod('DELETE')
             ->getForm()
-        ;
+            ;
     }
 }
